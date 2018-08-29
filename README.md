0c8b9009c97c9d13808c32ca0f2219da2482677c
Abdhesh Kumar <abdheshkumar90.rkg@gmail.com>
2018-08-30 00:27:56 +0100

    Added project description

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# scalajs-material-ui
+
+###This project is sample project structure to guide you, how to use scalajs,material-ui and react component facade and write react component in scalajs.
+####This project is categorized in three sections.
+1. **server** : It is akka-http based server. it's responsibility to server static content like css and scalajs compiled javascript file. If you want, you can any other server instead of akka-http. I love akka toolkit that's why I used akka-http server.
+
+2. **client** : It is scalajs based project. This project contains scalajs code, React component in ScalaJs, React Routes in scalajs and react facade. In this project, I also explained how to use nested routes in scalajs by very nice and clean way. *installNpm* sbt task used to install any npm dependencies from sbt instead of running *npm install* separately
+
+3. **ui** : It is not sbt based project. It is npm based project that contains react based components. I wrote facade for react based component in scalajs.
+
+####How to run this project?
+#####Start asset server
+1. Open terminal 
+2. sbt "project server" run  
+   It will start server on http://127.0.0.1:8063
+
+#####Compile react components
+1. Open terminal
+2. cd ui/users-app
+3. npm install (It will install all package.json dependencies)
+
+#####ScalaJs project
+1. Open terminal 
+2. sbt
+3. project client
+4. installNpm task on sbt
+5. fastOptJS (The compiled code will be moved to ui/apps/scalajs-material-ui)
+
+#####Access project
+1. Open browser
+2. Enter http://localhost:8063/pages/scalajs-material-ui/
+
+
+The good thing about this project structure, UI team can write react components in *ui/users-app* directory without disturbing Scala team. 
--- server/src/main/scala/AssetServer.scala
+++ server/src/main/scala/AssetServer.scala
@@ -15,25 +15,30 @@ object AssetServer extends App {
 
   val config: Config = ConfigFactory.load
 
-
   implicit val system: ActorSystem = ActorSystem("assets", config)
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val ec: ExecutionContextExecutor = system.dispatcher
 
   implicit def myRejectionHandler: RejectionHandler = {
-    RejectionHandler.newBuilder()
-      .handle { case MissingCookieRejection(cookieName) =>
-        complete(HttpResponse(BadRequest, entity = "No cookies, no service!!!"))
+    RejectionHandler
+      .newBuilder()
+      .handle {
+        case MissingCookieRejection(cookieName) =>
+          complete(
+            HttpResponse(BadRequest, entity = "No cookies, no service!!!")
+          )
       }
-      .handle { case AuthorizationFailedRejection =>
-        complete(Forbidden, "You're out of your depth!")
+      .handle {
+        case AuthorizationFailedRejection =>
+          complete(Forbidden, "You're out of your depth!")
       }
-      .handle { case ValidationRejection(msg, _) =>
-        complete(InternalServerError, "That wasn't valid! " + msg)
+      .handle {
+        case ValidationRejection(msg, _) =>
+          complete(InternalServerError, "That wasn't valid! " + msg)
       }
-      .handleAll[Rejection] {
-      _ => complete(MethodNotAllowed, s"Can't do that! Supported!")
-    }
+      .handleAll[Rejection] { _ =>
+        complete(MethodNotAllowed, s"Can't do that! Supported!")
+      }
       .handleNotFound {
         complete(NotFound, "Not here!")
       }
@@ -42,29 +47,47 @@ object AssetServer extends App {
 
   private val fileHandler = handleExceptions(ExceptionHandler {
     case _: NoSuchFileException =>
-      complete(HttpResponse(status = StatusCodes.InternalServerError, entity = HttpEntity(ContentTypes.`text/plain(UTF-8)`, "File Not Found")))
+      complete(
+        HttpResponse(
+          status = StatusCodes.InternalServerError,
+          entity =
+            HttpEntity(ContentTypes.`text/plain(UTF-8)`, "File Not Found")
+        )
+      )
   })
 
   def getPath(path: String): Path = java.nio.file.Paths.get(path).toAbsolutePath
 
   private[this] val static_base = config.getString("baseDirectory")
 
-  private val javascriptRoute: Route = path("js" / Segment / Segment) { (module, path) =>
-    encodeResponse {
-      val resourcePath = s"$static_base/apps/$module/$path"
-      fileHandler {
-        complete(HttpEntity(MediaTypes.`application/javascript` withCharset HttpCharsets.`UTF-8`, Files.readAllBytes(getPath(resourcePath))))
+  private val javascriptRoute: Route = path("js" / Segment / Segment) {
+    (module, path) =>
+      encodeResponse {
+        val resourcePath = s"$static_base/apps/$module/$path"
+        fileHandler {
+          complete(
+            HttpEntity(
+              MediaTypes.`application/javascript` withCharset HttpCharsets.`UTF-8`,
+              Files.readAllBytes(getPath(resourcePath))
+            )
+          )
+        }
       }
-    }
   }
 
-  private val cssRoute: Route = path("css" / Segment / Segment) { (module, path) =>
-    encodeResponse {
-      val resourcePath = s"$static_base/apps/$module/$path"
-      fileHandler {
-        complete(HttpEntity(MediaTypes.`text/css` withCharset HttpCharsets.`UTF-8`, Files.readAllBytes(getPath(resourcePath))))
+  private val cssRoute: Route = path("css" / Segment / Segment) {
+    (module, path) =>
+      encodeResponse {
+        val resourcePath = s"$static_base/apps/$module/$path"
+        fileHandler {
+          complete(
+            HttpEntity(
+              MediaTypes.`text/css` withCharset HttpCharsets.`UTF-8`,
+              Files.readAllBytes(getPath(resourcePath))
+            )
+          )
+        }
       }
-    }
   }
 
   private val staticPagesRoute: Route = pathPrefix("pages") {
@@ -72,29 +95,38 @@ object AssetServer extends App {
       path(Segments) { absUrl =>
         pathEndOrSingleSlash {
           val resourcePath = s"$static_base/apps/$module/index.html"
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, Files.readAllBytes(getPath(resourcePath))))
+          complete(
+            HttpEntity(
+              ContentTypes.`text/html(UTF-8)`,
+              Files.readAllBytes(getPath(resourcePath))
+            )
+          )
         }
       }
     }
   }
 
-  val route: Route = staticPagesRoute  ~ javascriptRoute  ~ cssRoute
+  val route: Route = staticPagesRoute ~ javascriptRoute ~ cssRoute
 
   val host = config.getString("host.ip")
   val httpPort = config.getInt("host.ports.http")
 
   /** Bind server for http request */
-  val bindingHttp = Http().bindAndHandle(route, host, httpPort)
+  val bindingHttp = Http()
+    .bindAndHandle(route, host, httpPort)
+    .map { f =>
+      println(s"Server running on ${f.localAddress}")
+      f
+    }
 
   sys.addShutdownHook(new Thread() {
     override def run() {
       val result = for {
         _ <- bindingHttp.flatMap(_.unbind())
       } yield ()
-      result.onComplete {
-        _ =>
-          system.terminate()
-          materializer.shutdown()
+      result.onComplete { _ =>
+        system.terminate()
+        materializer.shutdown()
       }
     }
   })
